abc22e0417be2c8cb9c1c83274dd49f1b643e15b
Matthew Holder <sixxgate@hotmail.com>
2019-12-23 20:05:16 -0600

    The power button will now shutdown the system

--- render/support/helpers.ts
+++ render/support/helpers.ts
@@ -1,3 +1,4 @@
+import subprocess from "child_process";
 
 export function toDataUrl(blob: Blob): Promise<string> {
     return new Promise<string>(function (resolve, reject): void {
@@ -13,14 +14,21 @@ export enum DBus {
     SESSION = "--session",
 }
 
-export function dbusSend(dbus: DBus, bus: string, objPath: string, ifName: string, member: string, ...args: string[]): string {
-    return `dbus-send ${dbus} --dest=${bus} ${objPath} ${ifName}.${member} ${args.join(" ")}`;
+export function dbusSend(dbus: DBus, bus: string, objPath: string, ifName: string, member: string, ...args: string[]): Promise<void> {
+    return new Promise((resolve, reject) => {
+        const cmd   = `dbus-send ${dbus} --print-reply --dest=${bus} ${objPath} ${ifName}.${member} ${args.join(" ")}`;
+        const child = subprocess.exec(cmd, (error, stdout, stderr) => {
+            stdout && console.log(stdout);
+            stderr && console.error(stderr);
+
+            error ? reject(error) : resolve();
+        });
+
+        child.unref();
+    });
 }
 
 export function signalShutdown(): Promise<void> {
-    const cmd = dbusSend(DBus.SYSTEM, "org.freedesktop.login1", "/org/freedesktop/login1",
+    return dbusSend(DBus.SYSTEM, "org.freedesktop.login1", "/org/freedesktop/login1",
         "org.freedesktop.login1.Manager", "PowerOff", "boolean:false");
-    console.log(cmd);
-
-    return Promise.resolve();
 }
